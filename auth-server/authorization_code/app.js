7dfb38a672706b5177f3a6b05b0278e9a3912551
mdemartino26 <55245840+mdemartino26@users.noreply.github.com>
2020-11-26 10:12:11 -0300

    link

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -104,7 +104,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('https://sethemood.vercel.app/dashboard/' +
+        res.redirect('https://sethemood.vercel.app/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
